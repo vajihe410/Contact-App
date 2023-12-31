f6c9f162dbc002cfd5950998467bf463d5685725
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-12-31 20:29:15 +0330

    add className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.module.css'
+import styles from './Header.module.css'
 
 function Header() {
   return (
-    <div>
+    <div className={styles.container}>
         <h1>Contact App</h1>
     </div>
   )
